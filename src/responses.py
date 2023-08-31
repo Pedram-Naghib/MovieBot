86da3b13069c0066b578144b9c6618b118e41878
pedram <pedramnaghib@gmail.com>
2023-08-31 15:08:43 +0300

    some fix

--- src/responses.py
+++ src/responses.py
@@ -130,11 +130,11 @@ def award_data(_id, title):
 
 #    0      1      2      3       4        5        6        7      8      9        10    11
 # (Title, imdbID, Year, Genre, Runtime, Director, Writers, Actors, Plot, Ratings, Poster, Type)
-@bot.inline_handler(lambda query: True)
+@bot.inline_handler(lambda query: True, bot_admin=True)
 def query_movies(inline_query: types.InlineQuery):
     queries = []
     id = 1
-    # try:
+
     title = inline_query.query
     if inline_query.query == '':
         title = None
