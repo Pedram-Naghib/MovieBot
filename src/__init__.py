ca7ad6f6a841dc4aa0603329df76376c3c0fdc35
pedram <pedramnaghib@gmail.com>
2023-08-31 15:00:36 +0300

    some fix

--- src/__init__.py
+++ src/__init__.py
@@ -26,7 +26,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://thickthot-bot.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://moviebotdream.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
